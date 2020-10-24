39db8b7af98403285f7c083966bd641bd6cb8199
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2020-10-24 18:08:33 -0300

    algoritmo andando pero las rutas mal
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- LaMejorRuta/src/Main.java
+++ LaMejorRuta/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
+		
+		
+		//Por defecto hay 6 nodos, los inicializo
 		Nodo nodo[] = new Nodo[6];
 
 		nodo[0] = new Nodo("A");
@@ -20,7 +24,6 @@ public class Main {
 
 		cargar(nodo);
 		mostrar_nodos(nodo);
-		// dijkstra(nodo, 0, 4);
 		
 		String opcion = "";
 		System.out.print("Por cual nodo desea empezar ?\n");
@@ -66,42 +69,6 @@ public class Main {
 		}
 	}
 
-	/*
-	 * public static void dijkstra(Nodo nodo[], int origen, int numeronodos) { int
-	 * largo= (numeronodos - 1 )*2; String matrizcostos[][] = new
-	 * String[largo][largo]; String vector[] = new String[largo]; //for(int i=0;
-	 * i<numeronodos; i++) //tengo que hacer sumar nodos hasta el numero de nodos -1
-	 * int i = 0; //hago la prueba solo para el nodo origen "i"-> indica cuantos
-	 * nodos adicinales coloco vector = calcularCostos(origen, nodo, vector, i);
-	 * mostrarVector(vector); escribirMatriz(matrizcostos,vector,i);
-	 * mostrarMatriz(matrizcostos,largo); }
-	 * 
-	 * public static String[] calcularCostos(int origen, Nodo nodo[], String
-	 * vector[], int nronododisponibles) { Nodo nodosrc = nodo[origen]; String
-	 * src,dst; int metrica=0,offset=0; //calculamos las metricas de cada enlace
-	 * dentro del nodo for (int j = 0; j < 6; j++) { if
-	 * (nodosrc.enlace[j].getNodoDestino() != null) { src =
-	 * nodosrc.enlace[j].getNodoOrigen().getNombre(); //obtengo nodo origen dst =
-	 * nodosrc.enlace[j].getNodoDestino().getNombre(); //obtengo nodo dst metrica =
-	 * nodosrc.enlace[j].getMetrica(); vector[offset] = src + "->" + dst;
-	 * vector[offset+1] = metrica+""; offset+=2; } } return vector; }
-	 * 
-	 * public static void mostrarVector(String vector[]){ for (int j=0;
-	 * j<vector.length;j++){ if(vector[j] != null) System.out.println(vector[j]
-	 * +"   "+ vector[j+1]); j+=1; } }
-	 * 
-	 * public static String [][] escribirMatriz(String matrizcostos[][],String
-	 * vector[],int cantNodo) {
-	 * 
-	 * for (int j = 0; j < vector.length; j++) matrizcostos [cantNodo][j] =
-	 * vector[j]; return matrizcostos; }
-	 * 
-	 * public static void mostrarMatriz(String matrizcostos [][],int size){
-	 * System.out.println("matriz"); for (int i = 0; i < size; i++) for (int j = 0;
-	 * j < size; j++) if(matrizcostos[i][j]!= null)
-	 * System.out.println(matrizcostos[i][j]); }
-	 */
-
 	public static void algoritmo(Nodo nodoOrigen, Nodo[] nodos) {
 		
 		List<Enlace> vecinos = nodoOrigen.getVecinos();
@@ -112,9 +79,18 @@ public class Main {
 			// Seteo el peso temporal del nodo
 			int pesoTemporal = vecinos.get(i).getMetrica() + nodoOrigen.getPesoFinal();
 			//verifico si la ruta que voy a setear es mejor que la anterior
-			if (pesoTemporal < nodos[numeroNodo].getPesoTemporal()) {
+			if (pesoTemporal < nodos[numeroNodo].getPesoTemporal()&&(!nodos[numeroNodo].isOrigen())) {
 				nodos[numeroNodo].setPesoTemporal(pesoTemporal);			
-				List<Nodo> camino = nodoOrigen.getCamino();
+				List<Nodo> camino = new ArrayList();
+				
+				
+				for(int j = 0; j< nodoOrigen.getCamino().size(); j++) {
+					Nodo aux = new Nodo("");
+					aux.setNombre(nodoOrigen.getCamino().get(j).getNombre());
+					camino.add(aux);
+				}
+				
+						
 				camino.add(nodoOrigen);	
 				nodos[numeroNodo].setCamino(camino);
 			}
@@ -157,9 +133,6 @@ public class Main {
 				System.out.print(nodo[i].getCamino().get(j).getNombre() + "\t");
 			}
 			System.out.print("\n");
-		}
-		
-		
+		}		
 	}
-
 }
